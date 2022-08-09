732696bb5a9c4f049dd0645d7de286072fb13e59
ElenaG <freshelf@yandex.ru>
2022-08-09 19:00:51 +0500

    add module of int in Task002

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,12 +1,13 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите целое положительное число");
+Console.WriteLine("Введите целое число");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number < 100)
+int number1 = Math.Abs(number);
+if(number1 < 100)
     Console.WriteLine("Третьей цифры нет");
 else
 {
-    string str_number = number.ToString();
+    string str_number = number1.ToString();
     char digit = str_number[2];
     string new_str_number = digit.ToString();
     int new_number = Convert.ToInt32(new_str_number);
