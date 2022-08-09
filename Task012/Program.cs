b6a4fe13e0d824e5d5e1a2be27e87a2a069f3ce4
ElenaG <freshelf@yandex.ru>
2022-08-09 22:03:28 +0500

    add another solution for Task002

---
+++ Task012/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = Math.Abs(number);
+if(number1 < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (number1 >= 1000) number1 /= 10;
+    int result = number1 % 10;
+    Console.WriteLine(result);
+}
