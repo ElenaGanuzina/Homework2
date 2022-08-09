707abe05c8190fa59967adf33a2d0497e4648b14
ElenaG <freshelf@yandex.ru>
2022-08-09 12:56:15 +0500

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+string str_number = number.ToString();
+char digit = str_number[1];
+string new_str_number = digit.ToString();
+int new_number = Convert.ToInt32(new_str_number);
+Console.WriteLine(new_number);
